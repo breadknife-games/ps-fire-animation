960988dc5b4c0200721946c12bd40d77e5309b2d
Ivy Jones <ivy@nosk.gg>
2025-11-25 00:47:31 -0700

    :sparkles: Add themes

--- webview-ui/src/index-webview-svelte.ts
+++ webview-ui/src/index-webview-svelte.ts
@@ -1,7 +1,11 @@
-import { mount } from "svelte";
-import "./app.css";
-import App from "./main-webview.svelte";
+import { mount } from 'svelte'
+import './app.css'
+import App from './main-webview.svelte'
+import { initTheme } from './stores/themeStore'
+
+// Initialize theme from localStorage or default
+initTheme()
 
 mount(App, {
-  target: document.getElementById("app")!,
-});
+    target: document.getElementById('app')!
+})
