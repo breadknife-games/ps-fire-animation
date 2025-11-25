ec5d878efb78fa4572fccc665866ca4d52bf11cd
Ivy Jones <ivy@nosk.gg>
2025-11-25 00:58:06 -0700

    :sparkles: Update expand icons

---
+++ webview-ui/src/lib/components/icons/IconDisclosureRight.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let className = ''
+    export {className as class}
+</script>
+
+<svg class={className} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+    <title>Disclosure Right</title>
+    <path class="fill" d="M6 4.5L12.5 9L6 13.5V4.5Z"/>
+</svg>
--- webview-ui/src/timeline/components/RowExpander.svelte
+++ webview-ui/src/timeline/components/RowExpander.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import IconChevronDown from '../../lib/components/icons/IconChevronDown.svelte'
     import IconChevronRight from '../../lib/components/icons/IconChevronRight.svelte'
+    import IconDisclosureDown from '../../lib/components/icons/IconDisclosureDown.svelte'
+    import IconDisclosureRight from '../../lib/components/icons/IconDisclosureRight.svelte'
 
     const {
         expanded: expandedProp = false,
+        expanderType = 'chevron',
         onToggle = () => {}
     } = $props<{
         expanded?: boolean
+        expanderType?: 'chevron' | 'disclosure'
         onToggle?: (value: boolean) => void
     }>()
 
@@ -27,9 +31,17 @@
     type="button"
     class="flex items-center gap-1 rounded px-1 py-0.5 text-timeline-muted transition hover:text-timeline-foreground focus-visible:outline focus-visible:outline-timeline-border"
     onclick={toggle}>
-    {#if expanded}
-        <IconChevronDown class="h-3 w-3 fill-current" />
+    {#if expanderType === 'disclosure'}
+        {#if expanded}
+            <IconDisclosureDown class="h-3 w-3 fill-current" />
+        {:else}
+            <IconDisclosureRight class="h-3 w-3 fill-current" />
+        {/if}
     {:else}
-        <IconChevronRight class="h-3 w-3 fill-current" />
+        {#if expanded}
+            <IconChevronDown class="h-3 w-3 fill-current" />
+        {:else}
+            <IconChevronRight class="h-3 w-3 fill-current" />
+        {/if}
     {/if}
 </button>
